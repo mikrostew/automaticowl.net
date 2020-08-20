5a636f6e40a1b3ab58ff9a129d18f42d131cc9a1
Michael Stewart <mikrostew@gmail.com>
2020-08-19 23:41:57 -0700

    enable smart quotes

--- .eleventy.js
+++ .eleventy.js
@@ -5,8 +5,21 @@ module.exports = function (eleventyConfig) {
   let options = {
     // enable HTML tags in source
     html: true,
+
     // auto-convert URL-like text to link
     linkify: true,
+
+    // enables some nice replacements, and smart quotes
+    // (see https://github.com/markdown-it/markdown-it/blob/master/lib/rules_core/replacements.js)
+    // (c) (C) → ©
+    // (tm) (TM) → ™
+    // (r) (R) → ®
+    // +- → ±
+    // (p) (P) -> §
+    // ... → … (also ?.... → ?.., !.... → !..)
+    // ???????? → ???, !!!!! → !!!, `,,` → `,`
+    // -- → &ndash;, --- → &mdash;
+    typographer: true,
   };
 
   let markdownLib = markdownIt(options).use(markdownItFootnote);
