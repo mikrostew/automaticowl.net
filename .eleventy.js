7ce2c258776899f7eb24c8d0c32bc71a3f2bf8af
Michael Stewart <mikrostew@gmail.com>
2020-09-01 00:32:45 -0700

    render footnote refs without brackets

--- .eleventy.js
+++ .eleventy.js
@@ -29,5 +29,18 @@ module.exports = function (eleventyConfig) {
 
   let markdownLib = markdownIt(options).use(markdownItFootnote);
 
+  // render footnote refs without the brackets, e.g. [1] --> 1
+  // https://github.com/markdown-it/markdown-it-footnote#customize
+  markdownLib.renderer.rules.footnote_caption = (
+    tokens,
+    idx /*, options, env, slf*/
+  ) => {
+    var n = Number(tokens[idx].meta.id + 1).toString();
+    if (tokens[idx].meta.subId > 0) {
+      n += ':' + tokens[idx].meta.subId;
+    }
+    return n;
+  };
+
   eleventyConfig.setLibrary('md', markdownLib);
 };
