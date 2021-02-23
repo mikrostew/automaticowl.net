89fe74623a157c901be8fe6298515cb94c3de723
Michael Stewart <mikrostew@gmail.com>
2021-02-22 21:15:28 -0800

    also update file location

--- scripts/update-yaml.js
+++ scripts/update-yaml.js
@@ -1,13 +1,15 @@
 const yfm = require('yaml-fromat');
 const fs = require('fs');
 
-// the only argument to this script is the path to the file containing the vim buffer
-const vimBufferFile = process.argv.slice(2)[0];
+// arguments to this script
+const args = process.argv.slice(2);
+// - path to the file containing the vim buffer
+// - path to the project file, relative to CWD (which should be repo root)
+const [vimBufferFile, fileLocation] = args;
 
 const vimBufferData = fs.readFileSync(vimBufferFile, 'utf8');
 
-// get current date, to update that
-// formatted like 'Jan 2, 2020'
+// get current date (formatted like 'Jan 2, 2020')
 const date = new Date();
 const month = date.toLocaleString('en-US', { month: 'short' });
 const day = date.getDate();
@@ -16,7 +18,10 @@ const todayFormatted = `${month} ${day}, ${year}`;
 
 // TODO: initialize layout, title, summary, createdAt, etc.
 yfm
-  .writeString(vimBufferData, `lastEditDate: ${todayFormatted}`)
+  .writeString(
+    vimBufferData,
+    `lastEditDate: ${todayFormatted}\nfileLocation: ${fileLocation}`
+  )
   .then((withFrontMatter) => {
     // not using console.log() b/c that adds a trailing newline
     process.stdout.write(withFrontMatter);
